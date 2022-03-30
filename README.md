0ed9d9a2050425189cf52d2b54ed425e1ed08402
Kgiunta <giunta.kyle@yahoo.com>
2022-03-29 21:22:52 -0400

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # WeatherWizard
+
+## Description
+
+Using third party API's this application displays the current weather outside as well as the five day future forecast and displays icons to let the user know weather conditions i.e sunny, cloudy rainy, etc.
+This application also lets the user know if the UV Index is high by highlighting after a certain threshold.
+
+## Table of Contents
+
+Below are the table of contents for this weather application.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+
+## Installation
+
+This code was developed from scratch. Using the third party API I created an API key to allow access to the data and then uploaded to Github.
+
+## Usage
+
+Input which city you would like to see in the search bar and watch the current weather populate as well as the five day forecast.
+
+Below is a screenshot of the application in use.
+
+![Weather Application showing NYC Weather](./Assets/screenshot.png)
+
+## Credits
+
+The following people helped bring this application alive:
+
+Jehyun Jung | Penn LPS Tutor <br>
+Dominick Albano |https://github.com/dev-dominick<Br>
+Issak Morales |https://github.com/CallMeIce<Br>
--- script.js
+++ script.js
@@ -6,11 +6,18 @@ $("#two").text(moment().add("days", 2).format("L"));
 $("#three").text(moment().add("days", 3).format("L"));
 $("#four").text(moment().add("days", 4).format("L"));
 $("#five").text(moment().add("days", 5).format("L"));
-var createButton = document.getElementById("createButton");
+// var createButton = document.getElementById("createButton");
 
-var cityNames = document.createElement("button");
-cityNames.textContent = userSearch;
-createButton.append(cityNames);
+var citys = document.getElementById("userInput").value;
+console.log(userSearch.value);
+localStorage.setItem("CitySearch", citys);
+var storeWeather = document.getElementById("userInput");
+var logDays = localStorage.getItem("CitySearch");
+storeWeather.textContent = logDays;
+
+// var cityNames = document.createElement("button");
+// cityNames.textContent = userSearch.value;
+// createButton.append(cityNames);
 
 // make a var by document.createlemt searchButton
 // city.textContent = input value
@@ -55,14 +62,13 @@ function fiveDays(cityName, lat, lon) {
     });
 }
 
-console.log(cityName);
-
 function weatherTodayRender(currentWeather, cityName) {
   var iconUrl = `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`;
   document.getElementById("jumbotron").textContent = cityName;
-  document.getElementById("temp").textContent = currentWeather.temp;
-  document.getElementById("hum").textContent = currentWeather.humidity;
-  document.getElementById("wind").textContent = currentWeather.wind_speed;
+  document.getElementById("temp").textContent = currentWeather.temp + " F";
+  document.getElementById("hum").textContent = currentWeather.humidity + " %";
+  document.getElementById("wind").textContent =
+    currentWeather.wind_speed + "MPH";
   document.getElementById("uv").textContent = currentWeather.uvi;
   console.log(uv);
   if (uv.innerHTML > 6) {
@@ -90,135 +96,91 @@ function weatherWeekRender(futureWeather) {
   var iconUrlOne = `https://openweathermap.org/img/w/${futureWeather[0].weather[0].icon}.png`;
   console.log(iconUrlOne);
   // document.getElementById("onetron").textContent = cityName;
-  document.getElementById("tempone").textContent = futureWeather[0].temp.day;
-  document.getElementById("humone").textContent = futureWeather[0].humidity;
-  document.getElementById("windone").textContent = futureWeather[0].wind_speed;
+  document.getElementById("tempone").textContent =
+    futureWeather[0].temp.day + " F";
+  document.getElementById("humone").textContent =
+    futureWeather[0].humidity + " %";
+  document.getElementById("windone").textContent =
+    futureWeather[0].wind_speed + " MPH";
   document.getElementById("uvone").textContent = futureWeather[0].uvi;
+  if (uvone.innerHTML > 6) {
+    document.getElementById("uvone").style.background = "red";
+  } else if (uvone.innerHTML < 6) {
+    document.getElementById("uvone").style.background = "green";
+  } else {
+  }
   document.getElementById("imgOne").setAttribute("src", iconUrlOne);
 
   var iconUrlTwo = `https://openweathermap.org/img/w/${futureWeather[1].weather[0].icon}.png`;
   console.log(iconUrlTwo);
   // document.getElementById("onetron").textContent = cityName;
-  document.getElementById("temptwo").textContent = futureWeather[1].temp.day;
-  document.getElementById("humtwo").textContent = futureWeather[1].humidity;
-  document.getElementById("windtwo").textContent = futureWeather[1].wind_speed;
+  document.getElementById("temptwo").textContent =
+    futureWeather[1].temp.day + " F";
+  document.getElementById("humtwo").textContent =
+    futureWeather[1].humidity + " %";
+  document.getElementById("windtwo").textContent =
+    futureWeather[1].wind_speed + " MPH";
   document.getElementById("uvtwo").textContent = futureWeather[1].uvi;
+  if (uvtwo.innerHTML > 6) {
+    document.getElementById("uvtwo").style.background = "red";
+  } else if (uvtwo.innerHTML < 6) {
+    document.getElementById("uvtwo").style.background = "green";
+  } else {
+  }
   document.getElementById("imgTwo").setAttribute("src", iconUrlTwo);
 
   var iconUrlThree = `https://openweathermap.org/img/w/${futureWeather[2].weather[0].icon}.png`;
   //   document.getElementById("onetron").textContent = cityName;
-  document.getElementById("tempthree").textContent = futureWeather[2].temp.day;
-  document.getElementById("humthree").textContent = futureWeather[2].humidity;
+  document.getElementById("tempthree").textContent =
+    futureWeather[2].temp.day + " F";
+  document.getElementById("humthree").textContent =
+    futureWeather[2].humidity + " %";
   document.getElementById("windthree").textContent =
-    futureWeather[2].wind_speed;
+    futureWeather[2].wind_speed + " MPH";
   document.getElementById("uvthree").textContent = futureWeather[2].uvi;
+  if (uvthree.innerHTML > 6) {
+    document.getElementById("uvthree").style.background = "red";
+  } else if (uvthree.innerHTML < 6) {
+    document.getElementById("uvthree").style.background = "green";
+  } else {
+  }
   document.getElementById("imgThree").setAttribute("src", iconUrlThree);
 
   var iconUrlFour = `https://openweathermap.org/img/w/${futureWeather[3].weather[0].icon}.png`;
   //   document.getElementById("onetron").textContent = cityName;
-  document.getElementById("tempfour").textContent = futureWeather[4].temp.day;
-  document.getElementById("humfour").textContent = futureWeather[4].humidity;
-  document.getElementById("windfour").textContent = futureWeather[4].wind_speed;
+  document.getElementById("tempfour").textContent =
+    futureWeather[4].temp.day + " F";
+  document.getElementById("humfour").textContent =
+    futureWeather[4].humidity + " %";
+  document.getElementById("windfour").textContent =
+    futureWeather[4].wind_speed + " MPH";
   document.getElementById("uvfour").textContent = futureWeather[4].uvi;
+  if (uvfour.innerHTML > 6) {
+    document.getElementById("uvfour").style.background = "red";
+  } else if (uvfour.innerHTML < 6) {
+    document.getElementById("uvfour").style.background = "green";
+  } else {
+  }
   document.getElementById("imgFour").setAttribute("src", iconUrlFour);
 
   var iconUrlFive = `https://openweathermap.org/img/w/${futureWeather[4].weather[0].icon}.png`;
   //   document.getElementById("onetron").textContent = cityName;
-  document.getElementById("tempfive").textContent = futureWeather[4].temp.day;
-  document.getElementById("humfive").textContent = futureWeather[4].humidity;
-  document.getElementById("windfive").textContent = futureWeather[4].wind_speed;
+  document.getElementById("tempfive").textContent =
+    futureWeather[4].temp.day + " F";
+  document.getElementById("humfive").textContent =
+    futureWeather[4].humidity + " %";
+  document.getElementById("windfive").textContent =
+    futureWeather[4].wind_speed + " MPH";
   document.getElementById("uvfive").textContent = futureWeather[4].uvi;
+  if (uvfive.innerHTML > 6) {
+    document.getElementById("uvfive").style.background = "red";
+  } else if (uvfive.innerHTML < 6) {
+    document.getElementById("uvfive").style.background = "green";
+  } else {
+  }
+
   document.getElementById("imgFive").setAttribute("src", iconUrlFive);
 }
 
-// function fourDays(cityName, lat, lon) {
-//   var dayTwoQueryone = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${APIKey}`;
-//   fetch(dayTwoQueryone)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (jsonInfo) {
-//       console.log(jsonInfo);
-//       weatherWeekRendertwo(jsonInfo.daily, cityName);
-//       // console.log(days.name);
-//     });
-// }
-// function weatherWeekRendertwo(futureWeather) {
-//   console.log(futureWeather);
-//   var iconUrlTwo = `https://openweathermap.org/img/w/${futureWeather[1].weather[0].icon}.png`;
-//   console.log(iconUrlOne);
-//   //   document.getElementById("onetron").textContent = cityName;
-//   document.getElementById("temptwo").textContent = futureWeather[1].temp.day;
-//   document.getElementById("humtwo").textContent = futureWeather[1].humidity;
-//   document.getElementById("windtwo").textContent = futureWeather[1].wind_speed;
-//   document.getElementById("uvtwo").textContent = futureWeather[1].uvi;
-//   document.getElementById("imgTwo").setAttribute("src", iconUrlTwo);
-// }
-// function fiveDay(cityName, lat, lon) {
-//   var fiveDayQuery = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${APIKey}`;
-//   fetch(fiveDayQuery)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (jsonInfo) {
-//       console.log(jsonInfo);
-//       weatherWeekRenderthree(jsonInfo.daily, cityName);
-//       // console.log(days.name);
-//     });
-// }
-// function weatherWeekRenderthree(futureWeather) {
-//   console.log(futureWeather);
-//   var iconUrlThree = `https://openweathermap.org/img/w/${futureWeather[2].weather[0].icon}.png`;
-//   //   document.getElementById("onetron").textContent = cityName;
-//   document.getElementById("tempthree").textContent = futureWeather[2].temp.day;
-//   document.getElementById("humthree").textContent = futureWeather[2].humidity;
-//   document.getElementById("windthree").textContent =
-//     futureWeather[2].wind_speed;
-//   document.getElementById("uvthree").textContent = futureWeather[2].uvi;
-//   document.getElementById("imgThree").setAttribute("src", iconUrlThree);
-// }
-// function fiveDay(cityName, lat, lon) {
-//   var fiveDayQuery = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${APIKey}`;
-//   fetch(fiveDayQuery)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (jsonInfo) {
-//       console.log(jsonInfo);
-//       weatherWeekRenderFour(jsonInfo.daily, cityName);
-//       // console.log(days.name);
-//     });
-// }
-// function weatherWeekRenderFour(futureWeather) {
-//   console.log(futureWeather);
-//   var iconUrlFour = `https://openweathermap.org/img/w/${futureWeather[4].weather[0].icon}.png`;
-//   console.log(iconUrlFour);
-//   //   document.getElementById("onetron").textContent = cityName;
-//   document.getElementById("tempfour").textContent = futureWeather[4].temp.day;
-//   document.getElementById("humfour").textContent = futureWeather[4].humidity;
-//   document.getElementById("windfour").textContent = futureWeather[4].wind_speed;
-//   document.getElementById("uvfour").textContent = futureWeather[4].uvi;
-//   document.getElementById("imgfour").setAttribute("src", iconUrlFour);
-// }
-
-// function weatherWeekRenderFive(futureWeather) {
-//   console.log(futureWeather);
-//   var iconUrlFive = `https://openweathermap.org/img/w/${futureWeather[4].weather[0].icon}.png`;
-//   console.log(iconUrlOne);
-//   document.getElementById("onetron").textContent = cityName;
-//   document.getElementById("tempfive").textContent = futureWeather[4].temp.day;
-//   document.getElementById("humfive").textContent = futureWeather[4].humidity;
-//   document.getElementById("windfive").textContent = futureWeather[4].wind_speed;
-//   document.getElementById("uvfive").textContent = futureWeather[4].uvi;
-//   document.getElementById("imgfive").setAttribute("src", iconUrlFive);
 // }
 searchButton.addEventListener("click", getInputFromSearch);
-
-// var storeWeather = document.getElementById("yourScore");
-// var getScore = localStorage.getItem("currentScore");
-// storeWeather.textContent = getScore;
-
-// make container
-// make a var by document.createlemt searchButton
-// city.textContent = input value
-// container.append(city)
